4beca7bb7de102c61357b79c8115388477e8a6b9
Lyle Mills <lylepmills@gmail.com>
2022-01-16 05:09:05 -0800

    Revert "update default"
    This reverts commit 0727c17aeec45ab4b12af2056f252c2f1af7e3e5.

--- rings/settings.cc
+++ rings/settings.cc
@@ -47,7 +47,7 @@ void Settings::Init() {
     data_.state.easter_egg = 0;
     data_.state.color_blind = 0;
     data_.state.frequency_locked = 0;
-    data_.state.phase_invert = 1;
+    data_.state.phase_invert = 0;
     data_.state.locked_transpose = 0.0f;
     data_.calibration_data.normalization_detection_threshold = 0.75f;
     freshly_baked_ = true;
