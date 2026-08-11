27df971e892eb57fe539748d42bd5c8bffbd7e7b
Lyle Mills <lylepmills@gmail.com>
2026-08-11 10:38:56 -0700

    Qualify Reed Pipe and Particle Burst fast FM

--- plaits/dsp/engine2/particle_burst_engine.h
+++ plaits/dsp/engine2/particle_burst_engine.h
@@ -311,6 +311,7 @@ class ParticleBurstEngine : public Engine {
       float* aux,
       size_t size,
       bool* already_enveloped);
+  virtual bool fast_fm_capable() const { return true; }
   virtual bool stereo_capable() const { return PLAITS_STEREO_PARTICLE_BURST; }
   virtual void HardSync() { Reset(); }
 
